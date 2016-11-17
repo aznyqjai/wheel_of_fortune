d54781da6fd03c0e3c3906403d1dad306c3e5098
aznyqjai <aznyqjai@gmail.com>
2016-11-17 11:19:38 -0500

    wrong modal not working

--- app.js
+++ app.js
@@ -1,144 +1,118 @@
 $(document).ready(function(){
 	console.log("$ firing");
-	//var tile= nFilter.setAttribute("style", "width:330px;float:left;")
-	draw_board();
-	// $("#intro").fadeOut(3000);
+	$("#game").hide();
 })
+
+
+
+function phrase(){
+	var phraselist=[
+	'ANGORA SWEATER',
+	'BEAUTIFUL GLASS VASE',
+	'BLUE JEANS',
+	'BRONZE TABLE LAMPS',
+	'CHRISTMAS WREATH',
+	'COMFORTABLE COUCH',
+	'COMFY LOAFERS',
+	'COMFY SOFA',
+	'CORDLESS PHONE',
+	'DECORATIVE AREA RUGS',
+	'DIMMER SWITCH',
+	'DOWN COMFORTER',
+	'ENGRAVED BATH SOAPS',
+	'FABRIC SOFTENER',
+	'FAMILY PHOTOS',
+	'FINE CHINA',
+	'FIRM PILLOWS',
+	'FLUFFY BATHROBE',
+	'FLUFFY PILLOWS',
+	'FOOTSTOOL',
+	'GARDENING GLOVES',
+	'GARDENING TOOLS',
+	'HOME THEATER SYSTEM',
+	'HORIZONTAL METAL SHED',
+	'HOT WATER HEATER',
+	'INCENSE BURNER',
+	'KITCHEN CABINETS',
+	'KITCHEN TABLE',
+	'LAWN SPRINKLER',
+	'MAHOGANY COFFEE TABLE',
+	'OUTDOOR RUGS',
+	'PORTABLE HEATER',
+	'STEAM MOP',
+	'PATIO LIGHTS',
+	'TOY TELEPHONE',
+	'WOODEN BOOKSHELVES'
+	];//home stuff
+
+	var num=Math.floor((Math.random() * phraselist.length) + 1);
+	return phraselist[num];
+}
+
+function Puzzle(){	
+	this.score=0;
+	this.phrase='';
+	this.char_found_times=0;
+
+
+	this.draw_board=function(phrase){
+		this.phrase=phrase.toUpperCase();
+		var tile='';
+		for (var i=0; i<phrase.length; i++){
+			if (phrase[i]===' '){
+				tile+= '<div id="tile'+i+'" class="space"><h2>&nbsp</h2></div>'
+			}
+
+			else {
+				tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp</h2></div>';
+			}
+		}
+		$("#tile_board").html(tile);
+		console.log(tile);
+	}
+
+	this.search=function(char){
+		var found_times=0;
+		char=($("#user_input").val()).toUpperCase();
+		console.log("search() fired  "+ char);
+		for (var i=0; i<this.phrase.length; i++){
+			if (char===this.phrase[i]){
+			$("#tile"+i).replaceWith('<div id="tile'+i+'" class="tile"><h2>'+char.toUpperCase()+'</h2></div>');	
+			this.char_found_times+=1;
+			this.score+=100;
+			}
+		}
+		console.log("testing found_times"+ this.char_found_times);
+		console.log("score is:  " + this.score);
+		$("#score_board").html(this.score);
+		$("#letters_played").append(char+ ",");
+		$("#user_input").val('');
+	}
+
+	this.solve=function(){
+		if (($("#solve").val().toUpperCase())==this.phrase){
+			for (var i=0; i<this.phrase.length; i++){
+			$("#tile"+i).replaceWith('<div id="tile'+i+'" class="tile"><h2>'+this.phrase[i]+'</h2></div>');	
+			}
+			console.log("great");
+		}else{
+			$("#wrong_modal").show();
+			console.log("wrong");
+		}
+	}
+}
+
+
 
 function start_game(){
 	$("#intro").fadeOut();
 	$("#game").fadeIn(2000);
+	puzzle.draw_board();
+	$("#wrong_modal").hide();
 }
 
-var phraselist=[
-'ANGORA SWEATER',
-'BEAUTIFUL GLASS VASE',
-'BLUE JEANS',
-'BRONZE TABLE LAMPS',
-'CHRISTMAS WREATH',
-'COMFORTABLE COUCH',
-'COMFY LOAFERS',
-'COMFY SOFA',
-'CORDLESS PHONE',
-'DECORATIVE AREA RUGS',
-'DIMMER SWITCH',
-'DOWN COMFORTER',
-'ENGRAVED BATH SOAPS',
-'FABRIC SOFTENER',
-'FAMILY PHOTOS',
-'FINE CHINA',
-'FIRM PILLOWS',
-'FLUFFY BATHROBE',
-'FLUFFY PILLOWS',
-'FOOTSTOOL',
-'GARDENING GLOVES',
-'GARDENING TOOLS',
-'HOME THEATER SYSTEM',
-'HORIZONTAL METAL SHED',
-'HOT WATER HEATER',
-'INCENSE BURNER',
-'KITCHEN CABINETS',
-'KITCHEN TABLE',
-'LAWN SPRINKLER',
-'MAHOGANY COFFEE TABLE',
-'OUTDOOR RUGS',
-'PORTABLE HEATER',
-'STEAM MOP',
-'PATIO LIGHTS',
-'TOY TELEPHONE',
-'WOODEN BOOKSHELVES'
-];//home stuff
-
-
-function spinner(){
-
-}
-
-
-var num=Math.floor((Math.random() * phraselist.length) + 1);
-var phrase=phraselist[num];
-var phrase_length=phrase.length;
-
-
-function draw_board(){
-	var tile='';
-	for (var i=0; i<phrase_length; i++){
-		if (phrase[i]===' '){
-			//$("#tile_board").append('<span id="tile'+i+'" class="space"><h2></h2></span>');
-			tile+= '<span id="tile'+i+'" class="space"><h2>&nbsp;</h2></span>'
-		}
-
-		else {
-			tile+='<span id="tile'+i+'" class="tile"><h2>&nbsp;</h2></span>';
-			//$("#tile_board").append('<span id="tile'+i+'" class="tile"><h2></h2></span>');
-		}
-	}
-	$("#tile_board").html(tile);
-}
-
-
-function new_puzzle(){
-	num=Math.floor((Math.random() * phraselist.length) + 1);
-	phrase=phraselist[num];
-	draw_board();
-}
-
-
-var user_entered=[];
-var counter=0;
-var score=0;
-
-function dosearch(){
-	var input = $("#user_input").val();
-	user_entered.push(input.toUpperCase());
-	counter++;
-
-	var found=false;
-	var found_times=0;
-
-	for (var i=0; i<phrase_length; i++){
-		if (input.toUpperCase()===phrase[i]){
-			$("#tile"+i).replaceWith('<span id="tile'+i+'" class="tile"><h2>'+input.toUpperCase()+'</h2></span>');
-			found=true;
-			found_times++;
-			score+=100;
-		}
-
-		// else {
-		// 	console.log("not found");
-		// 	// tile+='<span id="tile'+i+'" class="tile"><h2>&nbsp;</h2></span>';
-		// }
-
-		// $("#tile_board").html(tile);	
-	}
-
-	$("#score_board").html(score);
-
-	console.log("letter found?  " + found);
-	console.log("letter found "+ found_times + " times");
-	$("#user_input").val("");
-	console.log(counter);
-	console.log(user_entered);
-
-	if (counter==5){
-		$('#myModal').modal();
-	}
-}
-
-function solve(){
-	if (($("#solve").val()).toUpperCase()==phrase){
-		$("#solvetext").html('<h4> Congratuation you won</h4>');
-		console.log("you won");
-		score+=1000;
-		// setTimeout(function(){location.reload();}, 3000);
-
-	}
-	else {
-		$("#solvetext").html('<h4> YOU FAILED </h4>');
-		console.log("you lost");
-		// setTimeout(function(){location.reload();}, 3000);
-	}
-}
+var puzzle = new Puzzle();
+	
 
 
 
@@ -151,62 +125,161 @@ function solve(){
 
 
 
-/*
-var phrase="catcher in the rye is";
-var phrase="all good soliders run";
 
-var phrase_diced=phrase.split(""); 
-var phrase_length=phrase_diced.length;
-// var endpoing=0;
 
-// for (var i=0; i<phrase_length; i++){
-// 	if ((phrase[i]+phrase[i+1])>12){
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+// 	console.log("$ firing");
+// 	//var tile= nFilter.setAttribute("style", "width:330px;float:left;")
+// 	draw_board();
+// 	// $("#intro").fadeOut(3000);
+// })
+
+// function start_game(){
+// 	$("#intro").fadeOut();
+// 	$("#game").fadeIn(2000);
+// }
+
+// var phraselist=[
+// 'ANGORA SWEATER',
+// 'BEAUTIFUL GLASS VASE',
+// 'BLUE JEANS',
+// 'BRONZE TABLE LAMPS',
+// 'CHRISTMAS WREATH',
+// 'COMFORTABLE COUCH',
+// 'COMFY LOAFERS',
+// 'COMFY SOFA',
+// 'CORDLESS PHONE',
+// 'DECORATIVE AREA RUGS',
+// 'DIMMER SWITCH',
+// 'DOWN COMFORTER',
+// 'ENGRAVED BATH SOAPS',
+// 'FABRIC SOFTENER',
+// 'FAMILY PHOTOS',
+// 'FINE CHINA',
+// 'FIRM PILLOWS',
+// 'FLUFFY BATHROBE',
+// 'FLUFFY PILLOWS',
+// 'FOOTSTOOL',
+// 'GARDENING GLOVES',
+// 'GARDENING TOOLS',
+// 'HOME THEATER SYSTEM',
+// 'HORIZONTAL METAL SHED',
+// 'HOT WATER HEATER',
+// 'INCENSE BURNER',
+// 'KITCHEN CABINETS',
+// 'KITCHEN TABLE',
+// 'LAWN SPRINKLER',
+// 'MAHOGANY COFFEE TABLE',
+// 'OUTDOOR RUGS',
+// 'PORTABLE HEATER',
+// 'STEAM MOP',
+// 'PATIO LIGHTS',
+// 'TOY TELEPHONE',
+// 'WOODEN BOOKSHELVES'
+// ];//home stuff
+
+
+// var num=Math.floor((Math.random() * phraselist.length) + 1);
+// var phrase=phraselist[num];
+// var phrase_length=phrase.length;
+
+
+// function draw_board(phrase){
+// 	var tile='';
+// 	for (var i=0; i<phrase.length; i++){
+// 		if (phrase[i]===' '){
+// 			//$("#tile_board").append('<div id="tile'+i+'" class="space"><h2></h2></div>');
+// 			tile+= '<div id="tile'+i+'" class="space"><h2>&nbsp;</h2></div>'
+// 		}
+
+// 		else {
+// 			tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp;</h2></div>';
+// 			//$("#tile_board").append('<div id="tile'+i+'" class="tile"><h2></h2></div>');
+// 		}
+// 	}
+// 	$("#tile_board").html(tile);
+// }
+
+
+// function new_puzzle(){
+// 	num=Math.floor((Math.random() * phraselist.length) + 1);
+// 	phrase=phraselist[num];
+// 	draw_board();
+// }
+
+
+// var user_entered=[];
+// var counter=0;
+// var score=0;
+
+// function dosearch(){
+// 	var input = $("#user_input").val();
+// 	user_entered.push(input.toUpperCase());
+// 	counter++;
+
+// 	var found=false;
+// 	var found_times=0;
+
+// 	for (var i=0; i<phrase_length; i++){
+// 		if (input.toUpperCase()===phrase[i]){
+// 			$("#tile"+i).replaceWith('<div id="tile'+i+'" class="tile"><h2>'+input.toUpperCase()+'</h2></div>');
+// 			found=true;
+// 			found_times++;
+// 			score+=100;
+// 		}
+
+// 		// else {
+// 		// 	console.log("not found");
+// 		// 	// tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp;</h2></div>';
+// 		// }
+
+// 		// $("#tile_board").html(tile);	
+// 	}
+
+// 	$("#score_board").html(score);
+
+// 	console.log("letter found?  " + found);
+// 	console.log("letter found "+ found_times + " times");
+// 	$("#user_input").val("");
+// 	console.log(counter);
+// 	console.log(user_entered);
+
+// 	if (counter==5){
+// 		$('#myModal').modal();
 // 	}
 // }
-function break_on_letter(phrase){
-	// singles words should not br broken
-	// split on spaces where possible
-	// max letters in a row = 12 letters
-	// no phase is bigger than 24 characters
-	return 11; 
-}
-function draw_board(){
-	phrase_diced.forEach(function(element, index, arr) {
-	    //console.log('element: '+ element + ' index: '+ index);
-	    //console.log(arr);
-	    var new_line = (index == break_on_letter(phrase_diced)) ? 'new_line' : ''
-	    $("#title_board").append('<span id="tile'+index+'" class="tile ' + new_line + '"><h2>' + element + '</h2></span>');
-	});
-	//var tile='';
-	//for (var i=0; i<phrase_diced.length; i++){
-		// for (var j=0; j<phrase_diced[0].length; j++){
-		// 	// if (phrase_diced[i].length + phrase_diced[i+1].length>12){
-		// 	// 	//phrase_diced[i+1]
-		// 	// }
-		// 	// else {
- 	// 		$("#line1").append('<span id="tile'+j+'" class="tile"><h2></h2></span>');
-		// 	// }
-		// }
-	//}
-	
-	phrase_diced.forEach(function(element){
-		console.log("length of element  " + element.length);
-		for (var i=0; i<element.length; i++){
-			//if (element[])
-			$("#line1").append('<span id="tile'+i+'" class="tile"><h2></h2></span>');
-		}
-	})
-	
-}
-*/
 
+// function solve(){
+// 	if (($("#solve").val()).toUpperCase()==phrase){
+// 		$("#solvetext").html('<h4> Congratuation you won</h4>');
+// 		console.log("you won");
+// 		score+=1000;
+// 		// setTimeout(function(){location.reload();}, 3000);
 
-/*
-var a = ["a", "b", "c"];
+// 	}
+// 	else {
+// 		$("#solvetext").html('<h4> YOU FAILED </h4>');
+// 		console.log("you lost");
+// 		// setTimeout(function(){location.reload();}, 3000);
+// 	}
+// }
 
-a.forEach(function(element, index, arr) {
-    console.log('element: '+ element + ' index: '+ index);
-    console.log(arr);
-});
-*/
