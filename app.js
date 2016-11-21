b4db7251ce3dee8f469b43bb8674f82ce8a14e3f
aznyqjai <aznyqjai@gmail.com>
2016-11-21 00:11:42 -0500

    moved physical local files

--- app.js
+++ app.js
@@ -45,60 +45,78 @@ function phrase(){
 	'WOODEN BOOKSHELVES'
 	];//home stuff
 
-	var num=Math.floor((Math.random() * phraselist.length) + 1);
+	var num=Math.floor((Math.random() * phraselist.length)+1);
+	console.log(num);
 	return phraselist[num];
 }
 
-function Puzzle(){	
+function Puzzle(){
 	this.score=0;
 	this.phrase='';
 	this.char_found_times=0;
-
+	this.chars_searched=[];
+	// this.enoughpoint=false;
 
 	this.draw_board=function(phrase){
 		this.phrase=phrase.toUpperCase();
 		var tile='';
 		for (var i=0; i<phrase.length; i++){
 			if (phrase[i]===' '){
-				tile+= '<div id="tile'+i+'" class="space"><h2>&nbsp</h2></div>'
+				tile+= '<span id="tile'+i+'" class="space"><h2>&nbsp</h2></span>'
 			}
 
 			else {
-				tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp</h2></div>';
+				tile+='<span id="tile'+i+'" class="tile"><h2>&nbsp</h2></span>';
 			}
 		}
 		$("#tile_board").html(tile);
 		console.log(tile);
 	}
 
-	this.search=function(char){
-		var found_times=0;
-		char=($("#user_input").val()).toUpperCase();
-		console.log("search() fired  "+ char);
-		for (var i=0; i<this.phrase.length; i++){
-			if (char===this.phrase[i]){
-			$("#tile"+i).replaceWith('<div id="tile'+i+'" class="tile"><h2>'+char.toUpperCase()+'</h2></div>');	
-			this.char_found_times+=1;
-			this.score+=100;
+	this.search=function(){
+		var char=($("#user_input").val()).toUpperCase();
+
+	// if ((char=='A'||char=='E'||char=='I'||char=='O'||char=='U')){
+	//   this.score=this.score-100;
+	// }
+
+		if (char!=='' && this.chars_searched.indexOf(char)== -1){
+			this.chars_searched.push(char);
+			
+			console.log("search() fired  "+ char);
+			for (var i=0; i<this.phrase.length; i++){
+				if (char===this.phrase[i]){
+					$("#tile"+i).replaceWith('<span id="tile'+i+'" class="tile"><h2>'+char.toUpperCase()+'</h2></span>');
+					this.char_found_times+=1;
+					this.score+=100;  
+				}
 			}
+			// console.log("testing found_times"+ this.char_found_times);
+			console.log("score is:  " + this.score);
+			$("#score_board").html(this.score);
+			$("#letters_played").html(this.chars_searched);
+			$("#user_input").val('');
+			$("#rounds").html(this.chars_searched.length);
+			console.log(this.chars_searched);
 		}
-		console.log("testing found_times"+ this.char_found_times);
-		console.log("score is:  " + this.score);
-		$("#score_board").html(this.score);
-		$("#letters_played").append(char+ ",");
-		$("#user_input").val('');
+		else {
+			alert("Empty String Entered or Character Played");
+		}
 	}
 
 	this.solve=function(){
 		if (($("#solve").val().toUpperCase())==this.phrase){
 			for (var i=0; i<this.phrase.length; i++){
-				$("#tile"+i).replaceWith('<div id="tile'+i+'" class="tile"><h2>'+this.phrase[i]+'</h2></div>');	
+				if (phrase[i]!==' '){
+					$("#tile"+i).replaceWith('<span id="tile'+i+'" class="tile"><h2>'+this.phrase[i]+'</h2></span>');
+				}
 			}
 			$("#solve_puzzle_Modal").modal("hide");
 			$("#right_modal").modal("show");
 			$("#letters_played").html("");
 			console.log("great");
 		}
+
 		else{
 			$("#solve_puzzle_Modal").modal("hide");
 			$("#wrong_modal").modal("show");
@@ -116,12 +134,12 @@ function Puzzle(){
 		$("#intro").fadeOut();
 		$("#game").fadeIn(2000);
 		puzzle.draw_board();
-	// $("#wrong_modal").hide();
+		// $("#wrong_modal").hide();
 	}
 
 }
 var puzzle = new Puzzle();
-	
+
 
 
 
@@ -156,15 +174,15 @@ var puzzle = new Puzzle();
 
 
 // $(document).ready(function(){
-// 	console.log("$ firing");
-// 	//var tile= nFilter.setAttribute("style", "width:330px;float:left;")
-// 	draw_board();
-// 	// $("#intro").fadeOut(3000);
+//  console.log("$ firing");
+//  //var tile= nFilter.setAttribute("style", "width:330px;float:left;")
+//  draw_board();
+//  // $("#intro").fadeOut(3000);
 // })
 
 // function start_game(){
-// 	$("#intro").fadeOut();
-// 	$("#game").fadeIn(2000);
+//  $("#intro").fadeOut();
+//  $("#game").fadeIn(2000);
 // }
 
 // var phraselist=[
@@ -213,26 +231,26 @@ var puzzle = new Puzzle();
 
 
 // function draw_board(phrase){
-// 	var tile='';
-// 	for (var i=0; i<phrase.length; i++){
-// 		if (phrase[i]===' '){
-// 			//$("#tile_board").append('<div id="tile'+i+'" class="space"><h2></h2></div>');
-// 			tile+= '<div id="tile'+i+'" class="space"><h2>&nbsp;</h2></div>'
-// 		}
+//  var tile='';
+//  for (var i=0; i<phrase.length; i++){
+//    if (phrase[i]===' '){
+//      //$("#tile_board").append('<div id="tile'+i+'" class="space"><h2></h2></div>');
+//      tile+= '<div id="tile'+i+'" class="space"><h2>&nbsp;</h2></div>'
+//    }
 
-// 		else {
-// 			tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp;</h2></div>';
-// 			//$("#tile_board").append('<div id="tile'+i+'" class="tile"><h2></h2></div>');
-// 		}
-// 	}
-// 	$("#tile_board").html(tile);
+//    else {
+//      tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp;</h2></div>';
+//      //$("#tile_board").append('<div id="tile'+i+'" class="tile"><h2></h2></div>');
+//    }
+//  }
+//  $("#tile_board").html(tile);
 // }
 
 
 // function new_puzzle(){
-// 	num=Math.floor((Math.random() * phraselist.length) + 1);
-// 	phrase=phraselist[num];
-// 	draw_board();
+//  num=Math.floor((Math.random() * phraselist.length) + 1);
+//  phrase=phraselist[num];
+//  draw_board();
 // }
 
 
@@ -241,54 +259,54 @@ var puzzle = new Puzzle();
 // var score=0;
 
 // function dosearch(){
-// 	var input = $("#user_input").val();
-// 	user_entered.push(input.toUpperCase());
-// 	counter++;
+//  var input = $("#user_input").val();
+//  user_entered.push(input.toUpperCase());
+//  counter++;
 
-// 	var found=false;
-// 	var found_times=0;
+//  var found=false;
+//  var found_times=0;
 
-// 	for (var i=0; i<phrase_length; i++){
-// 		if (input.toUpperCase()===phrase[i]){
-// 			$("#tile"+i).replaceWith('<div id="tile'+i+'" class="tile"><h2>'+input.toUpperCase()+'</h2></div>');
-// 			found=true;
-// 			found_times++;
-// 			score+=100;
-// 		}
+//  for (var i=0; i<phrase_length; i++){
+//    if (input.toUpperCase()===phrase[i]){
+//      $("#tile"+i).replaceWith('<div id="tile'+i+'" class="tile"><h2>'+input.toUpperCase()+'</h2></div>');
+//      found=true;
+//      found_times++;
+//      score+=100;
+//    }
 
-// 		// else {
-// 		// 	console.log("not found");
-// 		// 	// tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp;</h2></div>';
-// 		// }
+//    // else {
+//    //  console.log("not found");
+//    //  // tile+='<div id="tile'+i+'" class="tile"><h2>&nbsp;</h2></div>';
+//    // }
 
-// 		// $("#tile_board").html(tile);	
-// 	}
+//    // $("#tile_board").html(tile);
+//  }
 
-// 	$("#score_board").html(score);
+//  $("#score_board").html(score);
 
-// 	console.log("letter found?  " + found);
-// 	console.log("letter found "+ found_times + " times");
-// 	$("#user_input").val("");
-// 	console.log(counter);
-// 	console.log(user_entered);
+//  console.log("letter found?  " + found);
+//  console.log("letter found "+ found_times + " times");
+//  $("#user_input").val("");
+//  console.log(counter);
+//  console.log(user_entered);
 
-// 	if (counter==5){
-// 		$('#myModal').modal();
-// 	}
+//  if (counter==5){
+//    $('#myModal').modal();
+//  }
 // }
 
 // function solve(){
-// 	if (($("#solve").val()).toUpperCase()==phrase){
-// 		$("#solvetext").html('<h4> Congratuation you won</h4>');
-// 		console.log("you won");
-// 		score+=1000;
-// 		// setTimeout(function(){location.reload();}, 3000);
+//  if (($("#solve").val()).toUpperCase()==phrase){
+//    $("#solvetext").html('<h4> Congratuation you won</h4>');
+//    console.log("you won");
+//    score+=1000;
+//    // setTimeout(function(){location.reload();}, 3000);
 
-// 	}
-// 	else {
-// 		$("#solvetext").html('<h4> YOU FAILED </h4>');
-// 		console.log("you lost");
-// 		// setTimeout(function(){location.reload();}, 3000);
-// 	}
+//  }
+//  else {
+//    $("#solvetext").html('<h4> YOU FAILED </h4>');
+//    console.log("you lost");
+//    // setTimeout(function(){location.reload();}, 3000);
+//  }
 // }
 
